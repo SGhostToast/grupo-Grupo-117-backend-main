9941952e1fc22d4be9751e29cc2d87d467f69598
GhostToast <sarratiaa00@gmail.com>
2023-06-01 19:25:03 -0400

    database woking

--- src/config/config.js
+++ src/config/config.js
@@ -7,21 +7,21 @@ const module_exports =  {
   "development": {
     "username": process.env.DB_USERNAME,
     "password": process.env.DB_PASSWORD,
-    "database": String(process.env.DB_NAME),
+    "database": `${process.env.DB_NAME}_development`,
     "host": process.env.DB_HOST,
     "dialect": "postgres"
   },
   "test": {
     "username": process.env.DB_USERNAME,
     "password": process.env.DB_PASSWORD,
-    "database": String(process.env.DB_NAME),
+    "database": `${process.env.DB_NAME}_test`,
     "host": process.env.DB_HOST,
     "dialect": "postgres"
   },
   "production": {
     "username": process.env.DB_USERNAME,
     "password": process.env.DB_PASSWORD,
-    "database": String(process.env.DB_NAME),
+    "database": `${process.env.DB_NAME}_production`,
     "host": process.env.DB_HOST,
     "dialect": "postgres"
   }
