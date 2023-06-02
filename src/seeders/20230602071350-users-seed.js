d8487f1909f20b8e470bb8a44efd1aef21a79bc7
GhostToast <sarratiaa00@gmail.com>
2023-06-02 03:25:34 -0400

    seed

---
+++ src/seeders/20230602071350-users-seed.js
@@ -0,0 +1,27 @@
+module.exports = {
+  up: (queryInterface) => queryInterface.bulkInsert('Users', [
+    {
+      username: 'ghosttoast',
+      password: 'ghosttoast123',
+      mail: 'ghosttoast@uc.cl',
+      played_matches: 0,
+      won_matches: 0,
+      max_score: 0,
+      total_score: 0,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },
+    {
+      username: 'lilianbernot',
+      password: 'lilianbernot123',
+      mail: 'lilianbernot@uc.cl',
+      played_matches: 0,
+      won_matches: 0,
+      max_score: 0,
+      total_score: 0,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }
+  ]),
+  down: (queryInterface) => queryInterface.bulkDelete('Users', null, {})
+};
