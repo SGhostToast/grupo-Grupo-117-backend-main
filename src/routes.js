38f31ba398f3caaea15e3e68f35df28e21e67ca7
GhostToast <sarratiaa00@gmail.com>
2023-06-02 03:56:54 -0400

    first gets and posts

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,12 @@
 const Router = require("koa-router");
 const characters = require("./routes/characters.js");
+const users = require("./routes/users.js");
 
 const router = new Router();
 
 // la ruta por los endpoint de characters.js sera /characters
 // la ruta del show : /characters/show
 router.use('/characters', characters.routes());
+router.use('/users', users.routes());
 
 module.exports = router;
